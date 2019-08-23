4ba90e7d088bc7f7be8fcec7f0a48ff213c58f1a
Peter Doggart <peter.doggart@b-secur.com>
2019-08-23 17:29:59 +0100

    Inital commit of matlab code.

--- load_sepsis_model.m
+++ load_sepsis_model.m
@@ -1,3 +1,9 @@
 function model = load_sepsis_model()
-    model = [];
+    % Load the cluster means
+    load('kmeans-cluster-centers-2.mat', 'C');
+    model.C = C;
+
+    % Load the compact ensemble
+    mdl = loadCompactModel('sepsisRUSBoostTree2.mat');
+    model.Mdl = mdl;
 end
